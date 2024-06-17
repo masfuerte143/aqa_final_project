56f688539ee0f8c37227a14d5a2fa4c0fcc9f5fc
MasFuerte <two_21@mail.ru>
2024-06-17 11:20:14 +0300

    added allure steps in regress cases

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
@@ -39,41 +40,49 @@ class MainPage(BasePage):
 
     # Actions and Methods
 
+    @allure.step("В большом слайдере отображается первый элемент")
     def check_visibility_of_big_slider_first_item(self):
         """Проверить видимость 1 элемента в большом слайдере"""
         self.get_big_slider_first_item().is_displayed()
 
+    @allure.step("В большом слайдере отображается второй элемент")
     def check_visibility_of_big_slider_second_item(self):
         """Проверить видимость 2 элемента в большом слайдере"""
         self.get_big_slider_second_item().is_displayed()
 
+    @allure.step("Пролистать большой слайдер вправо")
     def click_big_slider_next_button(self):
         """Пролистать вправо большой слайдер"""
         actions = ActionChains(self.driver)
         actions.move_to_element(self.get_big_slider_next_button()).perform()
         self.get_big_slider_next_button().click()
 
+    @allure.step("Пролистать большой слайдер влево")
     def click_big_slider_prev_button(self):
         """Пролистать влево большой слайдер"""
         actions = ActionChains(self.driver)
         actions.move_to_element(self.get_big_slider_prev_button()).perform()
         self.get_big_slider_prev_button().click()
 
+    @allure.step("В маленьком слайдере отображается первый элемент")
     def check_visibility_of_small_slider_first_item(self):
         """Проверить видимость 1 элемента в маленьком слайдере"""
         self.get_small_slider_first_item().is_displayed()
         BasePage.take_screenshot(self, ProjectPaths.screens_path, action_name="SmallSlider1item-OK")
 
+    @allure.step("В маленьком слайдере отображается второй элемент")
     def check_visibility_of_small_slider_second_item(self):
         """Проверить видимость 2 элемента в маленьком слайдере"""
         self.get_small_slider_second_item().is_displayed()
 
+    @allure.step("Пролистать маленький слайдер вправо")
     def click_small_slider_next_button(self):
         """Пролистать вправо маленький слайдер"""
         actions = ActionChains(self.driver)
         actions.move_to_element(self.get_small_slider_next_button()).perform()
         self.get_small_slider_next_button().click()
 
+    @allure.step("Пролистать маленький слайдер влево")
     def click_small_slider_prev_button(self):
         """Пролистать влево маленький слайдер"""
         actions = ActionChains(self.driver)
